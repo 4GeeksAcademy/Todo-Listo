5a0348797764b96b027793ff861c0c1c2953af1e
Antonio Gomez <totico388@hotmail.com>
2024-12-03 19:45:04 +0000

    Listo sin profundizar los estilos

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,62 @@
-import React from "react";
+import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+
+
 
 //create your first component
 const Home = () => {
+	const [tarea, setTarea] = useState("")
+	const [listaTareas, setListatareas] = useState([])
+	console.table(listaTareas)
+
+	function borrartarea(index) {
+		// aqui estamos almazenando un copia del array principal ya que puede genrar conflicto con el array principal 
+
+		const nuevalista = [...listaTareas]
+		nuevalista.splice(index, 1);
+		setListatareas(nuevalista)
+	}
+
+	let listadetareasnuevas = listaTareas.map((item, index) => <li key={index}>   {item} <span onClick={() => { console.log(index); borrartarea(index) }}> x </span> </li>)
+
+	function agregartarea(e) {
+
+		// Aqui estamos utilizando una condicion para 
+		if (e.key == "Enter") {
+
+			// Al array listaTareas le agregamos la informacion que recopila el input(Tarea)
+
+			// Creamos un nuevo array en el cual se lo incorporamos a la base de datos que recopila el input(Tarea)
+			let arraynuevo = listaTareas.concat(tarea)
+
+			// // Ahora aqui le pasamos el valor de arraynuevo a la funcion setListatareas
+
+			setListatareas(arraynuevo)
+
+			// quiero agregar lo que se esta escribiendo(Tarea) a una lista (Listatareas)
+
+			setTarea('')
+		}
+
+	}
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className=" p-3 mb-2 bg-light">
+			<div className="">
+
+				<div className=" cabecera fw-light "> <h1> TAREAS </h1></div>
+
+
+				<input type=" input text" onChange={(e) => setTarea(e.target.value)} value={tarea}
+					onKeyDown={agregartarea}
+				/>
+
+				<ul className="lista">
+					{listadetareasnuevas}
+				</ul>
+
+				<div> <p> {listaTareas.length} </p> </div>
+			</div>
 		</div>
 	);
 };
